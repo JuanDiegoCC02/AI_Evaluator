84ac9f48afc66eea1a8b8d2c6975bea924652737
Juan Diego C.C. <dcorella2324@gmail.com>
2026-06-08 16:57:40 -0600

    feat: create evaluator url in config/urls.py

--- evaluator/urls.py
+++ evaluator/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import EvaluationListCreateView
 
 
-urlspatterns = [
+urlpatterns = [
     path(
         "evaluations/",
         EvaluationListCreateView.as_view(),
