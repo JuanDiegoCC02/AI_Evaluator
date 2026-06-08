d740f708c6aa4d94afb680c4fb00fe6cfb672a40
Juan Diego C.C. <dcorella2324@gmail.com>
2026-06-08 16:35:33 -0600

    feat: create EvaluationListCreateView in Views

--- evaluator/views.py
+++ evaluator/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Evaluation
+from .serializers import EvaluationSerializer
 
 # Create your views here.
+
+class EvaluationListCreateView(generics.ListCreateAPIView):
+    queryset = Evaluation.objects.all()
+    serializer_class = EvaluationSerializer
+    
