1f9bad38ff8b628ff43726878dec0449d65d3851
Juan Diego C.C. <dcorella2324@gmail.com>
2026-06-08 16:15:57 -0600

    feat: create evaluation model

--- evaluator/models.py
+++ evaluator/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+#model for evaluation 
+class Evaluation(models.Model):
+    questionb = models.TextField()
+    answer = models.TextField()
+
+    grammar_score = models.FloatField(default=0)  
+    relevance_score = models.FloatField(default=0)
+    final_score = models.FloatField(default=0)
+
+    grammar_feedback = models.TextField(blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"Evaluation {self.id}"
