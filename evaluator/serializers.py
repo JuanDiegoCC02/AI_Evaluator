b22bc5738cd6ab18f07d2af173448c35dcc919a3
Juan Diego C.C. <dcorella2324@gmail.com>
2026-06-09 00:05:56 -0600

    feat: update serializers in EvaluationSerializer new technical data

--- evaluator/serializers.py
+++ evaluator/serializers.py
@@ -1,8 +1,11 @@
+import time
+
 from rest_framework import serializers
 from .models import Evaluation
 
 from .services.grammar import evaluate_grammar
 from .services.relevance import evaluate_relevance
+
 
 
 class EvaluationSerializer(serializers.ModelSerializer):
@@ -11,15 +14,22 @@ class EvaluationSerializer(serializers.ModelSerializer):
         model = Evaluation
         fields = '__all__'
         read_only_fields = (
-            'grammar_score',
-            'relevance_score',
-            'final_score',
-            'grammar_feedback',
-            'created_at',
-            'evaluation_label'
+        'grammar_score',
+        'relevance_score',
+        'final_score',
+        'grammar_feedback',
+        'created_at',
+        'evaluation_label',
+        'processing_time',
+        'language',
+        'embedding_model',
+        'grammar_errors',
+        'response_length',
         )
 
     def create(self, validated_data):
+
+        start_time = time.time()
 
         question = validated_data['question']
         answer = validated_data['answer']
@@ -27,16 +37,26 @@ class EvaluationSerializer(serializers.ModelSerializer):
         # Grammar evaluation
         grammar_result = evaluate_grammar(answer)
 
+        grammar_errors = len(grammar_result['feedback'])
+
+        response_length = len(answer.split())
+
         grammar_score = grammar_result['score']
 
         grammar_feedback = "\n".join(
             grammar_result['feedback']
         )
+    
 
         # Semantic relevance
         relevance_score = evaluate_relevance(
             question,
             answer
+        )
+
+        processing_time = round(
+            time.time() - start_time,
+            3
         )
 
         # Final score
@@ -65,7 +85,12 @@ class EvaluationSerializer(serializers.ModelSerializer):
             relevance_score=relevance_score,
             final_score=final_score,
             grammar_feedback=grammar_feedback,
-            evaluation_label=evaluation_label
+            evaluation_label=evaluation_label,
+            processing_time=processing_time,
+            language="en-US",
+            embedding_model="all-MiniLM-L6-v2",
+            grammar_errors=grammar_errors,
+            response_length=response_length
         )
 
         return evaluation
